be0b03dc2047089d465db11b9e23da05594656fd
G. Uitslag <klapinklapin@gmail.com>
2023-06-16 23:25:22 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author G. Uitslag <klapinklapin@gmail.com>
+ */
+$lang['template']              = '— Template';
+$lang['not_found']             = ' niet gevonden.';
+$lang['processing_template']   = '— Verwerken van de template ';
+$lang['stopped_recursion']     = ' gestopt vanwege recursieve aanroep.';
+$lang['no_permissions_view']   = '<div class="templater"> Geen toestemming om de template weer te geven </div>';
+$lang['no_such_section']       = '<div class="templater">— Er is geen sectie "%1$s" in deze pagina ';
