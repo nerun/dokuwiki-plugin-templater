c68642ce69f2f20241111f3d69c535ecb9061928
Aleksandr Selivanov <alexgearbox@yandex.ru>
2023-06-17 19:15:23 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['template']              = 'Шаблон';
+$lang['not_found']             = 'не найден.';
+$lang['processing_template']   = 'Обработка шаблона';
+$lang['stopped_recursion']     = 'остановлена из-за рекурсии.';
+$lang['no_permissions_view']   = 'Нет прав для просмотра шаблона';
+$lang['no_such_section']       = 'В шаблоне нет раздела "%1$s":';
