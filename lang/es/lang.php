8f022b10ff80a814132668f5e2c9916c9468861c
Daniel Dias Rodrigues <danieldiasr@gmail.com>
2023-06-17 18:35:32 +0200

    translation update

---
+++ lang/es/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ */
+$lang['template']              = 'El template';
+$lang['not_found']             = 'no fue encontrado.';
+$lang['processing_template']   = 'El procesamiento del template';
+$lang['stopped_recursion']     = 'se detuvo debido a la recursividad.';
+$lang['no_permissions_view']   = 'Sin permisos para ver el template';
+$lang['no_such_section']       = 'No existe tal sección "%1$s" en la página';
