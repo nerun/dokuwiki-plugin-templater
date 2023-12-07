5130b53637a023f6003e129727ff0c84cb59d09a
粗音未来 <miku39@onsei.fans>
2023-12-07 09:30:23 +0100

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 粗音未来 <miku39@onsei.fans>
+ */
+$lang['template']              = '模板';
+$lang['not_found']             = '未找到。';
+$lang['processing_template']   = '模板';
+$lang['stopped_recursion']     = '由于递归停止了运行。';
+$lang['no_permissions_view']   = '没有权限阅览该模板';
+$lang['no_such_section']       = '页面中没有"%1$s"区域';
