b9311528344aa2fec23a1bfb3dac71e3cf130a8a
nerun <danieldiasr@gmail.com>
2023-12-14 17:34:49 -0300

    Better debugger.

--- debug.php
+++ debug.php
@@ -1,10 +1,15 @@
 <?php
 /**
  * Daniel Dias Rodrigues (aka Nerun) <danieldiasr@gmail.com>
+ * add to script:
+ * require_once('debug.php');
  */
-function printr($arr) {
-	echo "<pre>";
+function printr($arr, $name=null) {
+	echo '<pre>';
+	if (!empty($name)){
+	    echo $name.' =<br />';
+	}
 	print_r($arr);
-	echo "</pre><br><br>";
+	echo '</pre><br />';
 }
 ?>
