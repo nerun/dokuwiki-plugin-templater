f6e508ecafdc0f38a7b2d1114a2e596aab270c34
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:26:08 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['template']              = 'Šablóna';
+$lang['not_found']             = 'sa nenašla.';
+$lang['processing_template']   = 'Spracovanie šablóny';
+$lang['stopped_recursion']     = 'zastavené kvôli rekurzii.';
+$lang['no_permissions_view']   = 'Žiadne oprávnenia na zobrazenie šablóny';
+$lang['no_such_section']       = 'Sekcia "%1$s" na stránke neexistuje';
